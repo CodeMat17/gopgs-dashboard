fcc0f544ee7891ce6171ede49429c5e48f13a44f
CodeMat17 <codemat.biz@gmail.com>
2025-04-10 21:02:34 +0100

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="w-full min-h-screen px-4 py-32 text-center space-y-5">
-      <h2 className="font-semibold text-2xl">Welcome to the GOUNI Postgraduate School Website Dashboard</h2>
+      <h2 className="font-semibold text-2xl sm:text-4xl lg:text-5xl">Welcome to the GOUNI Postgraduate School Website Dashboard</h2>
       <p className="text-lg text-muted-foreground">Use the left navigation pan to jump through the pages to create, delete or update info.</p>
       </div>
   );
